8b140889484f3940e155e01b58c7772fedf837c1
Ben Manning <bmanning@me.com>
2015-11-20 11:57:25 -0800

    add rspec and rb files

---
+++ week-06-intro-ruby-on-rails/day-02/module-02/username.rb
@@ -0,0 +1,14 @@
+# Make sure to run the tests in your /spec folder
+# Run `rspec /spec/username_spec.rb` to get started.
+
+def format_name(first, last)
+  nil
+end
+
+def format_year
+  nil
+end
+
+def build_username
+  nil
+end
