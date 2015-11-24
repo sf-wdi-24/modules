146a8c87690a37cf889f1d7c87a6527b2c18e04c
Ben Manning <bmanning@me.com>
2015-11-24 11:09:35 -0800

    add alternate solution

--- week-06-intro-ruby-on-rails/day-04/drill/solution.rb
+++ week-06-intro-ruby-on-rails/day-04/drill/solution.rb
@@ -30,22 +30,24 @@ def bubble_sort(array)
 end
 
 
-# def bubble_sort(array)
-#       swapped = true
-#       length = array.length-1
-#       while swapped do
-#           i = 0
-#           swapped = false
-#           while i <length
-#               n = i +1
-#               if array[i] > array[n]
-#                   temp = array[i]
-#                   array[i]= array[n]
-#                   array[n] = temp
-#                   swapped = true
-#               end
-#              i+=1
-#           end
-#       end
-#       return array
-# end
+
+# an alternate solution
+def bubble_sort(array)
+      swapped = true
+      length = array.length-1
+      while swapped do
+          i = 0
+          swapped = false
+          while i <length
+              n = i +1
+              if array[i] > array[n]
+                  temp = array[i]
+                  array[i]= array[n]
+                  array[n] = temp
+                  swapped = true
+              end
+             i+=1
+          end
+      end
+      return array
+end
