1572e51f7585bd3dc26da052c7b65a1d5d0a6583
Ben Manning <bmanning@me.com>
2015-12-01 15:22:25 -0800

    delete files from the lesson

--- week-06-intro-ruby-on-rails/day-04/module-02/4_model_speakers.rb
+++
@@ -1,9 +0,0 @@
-require 'ffaker'
-require './models/speaker.rb'
-
-5.times do
-  first = FFaker::Name.last_name
-  last = FFaker::Name.first_name
-  email = FFaker::Internet.free_email
-  # speaker = Speaker.new({})
-end
